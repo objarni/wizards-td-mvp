d12ca2d95e4a4526d35160aec248717c3845f9fd
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-08-17 16:38:54 +0200

    Use element factory function

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,29 @@
 module Main exposing (main)
 
+import Browser exposing (element)
 import Html exposing (text)
 
 
 main =
-    text "hello world"
+    element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init () =
+    ( (), Cmd.none )
+
+
+subscriptions model =
+    Sub.none
+
+
+update msg model =
+    ( model, Cmd.none )
+
+
+view _ =
+    text "hello world!"
